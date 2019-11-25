59a672dc7e2995f7dc2d3b073c4671977c7f95b5
Chris Alegre <calegre14@gmail.com>
2019-11-25 11:30:42 -0700

    Added Models, Controllers, API

---
+++ TourMore/ModelControllers/URLConstants/UrlConstants.swift
@@ -0,0 +1,21 @@
+//
+//  UrlConstants.swift
+//  TourMore
+//
+//  Created by Christopher Alegre on 11/21/19.
+//  Copyright © 2019 Christopher Alegre. All rights reserved.
+//
+
+import Foundation
+
+struct URLConstants {
+    //Components
+    static let baseURL = "https://api.yelp.com/v3"
+    static let business = "businesses"
+    static let search = "search"
+    
+    //Query
+    static let location = "location"
+    
+    static let auhorizationToken = "Bearer RBUMAAZ21zQ69-DPwd03VPLzl1Shk1CoFEE9UpQTLRLhOiMQvcp3l8J02ez3ZufOPdAGbZJaFWWRXC6QD6VPMJD-RvlHEuVYGC6cKyh22YUHl6rR14cLHTP6HTHTXXYx"
+}
---
+++ TourMore/TourMore/Models/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  TourMore
+//
+//  Created by Christopher Alegre on 11/22/19.
+//  Copyright © 2019 Christopher Alegre. All rights reserved.
+//
+
+import UIKit
+
+class User {
+    
+    //Save Business ID from model
+    var favoritesID: [String]?
+    var name: String
+    var uid: String
+    var bio: String?
+    var profilePicture: UIImage?
+    var comment: [Comment]?
+    
+    
+    init(favoritesID: [String]? = [], comment: [Comment]? = [], name: String, uid: String, bio: String?, profilePicture: UIImage?) {
+        self.favoritesID = favoritesID
+        self.comment = comment
+        self.name = name
+        self.uid = uid
+        self.bio = bio
+        self.profilePicture = profilePicture
+    }
+}
