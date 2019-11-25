3b4a8c7b4ae646012bb292239a0c3106fc7009c4
Chris Alegre <calegre14@gmail.com>
2019-11-25 16:23:19 -0700

    Added User model, added part user Controller

---
+++ TourMore/TourMore/Controllers/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  TourMore
+//
+//  Created by Christopher Alegre on 11/25/19.
+//  Copyright © 2019 Christopher Alegre. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
