3d01f8c634be6347e60aa0d9062dd373c6cf0d84
Michael Di Cesare <mvdicesare@gmail.com>
2019-11-26 12:09:48 -0700

    updated story boards

--- TourMore/TourMore/Controllers/ModelControllers/CreatedLocationController.swift
+++ TourMore/TourMore/Controllers/ModelControllers/CreatedLocationController.swift
@@ -15,39 +15,39 @@ class CreatedLocationController {
     
     
     // MARK: - CRUD
-    func addNewLocation(businessName: String, address1: String, address2: String, city: String, zipCode: String, completion: @escaping (CreatedLocation?, Error?) -> ()) {
-        
-        reauthenticateUser(with: AuthCredential) { (authCredential, error) in
-            if let _ = error {
-                completion(nil, error)
-        }
-            guard let authCredential = authCredential else { completion(nil,nil) ; return}
-        }
-    }
-    
-    func deleteLocation() {
-        
-    }
-    
-    func fetchLocation() {
-        
-    }
-    
-    // MARK: - methods
-    
-    private func reauthenticateUser(with credential: AuthCredential, completion: @escaping (AuthCredential?, Error?) -> Void ) {
-        let user = Auth.auth().currentUser
-        let credential: AuthCredential
-        user?.reauthenticate(with: credential, completion: { (credential, error) in
-            if let error = error {
-                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
-                completion(nil, error)
-            } else {
-                print("User is logged in")
-                completion(credential?.credential, nil)
-            }
-        })
-    }
+//    func addNewLocation(businessName: String, address1: String, address2: String, city: String, zipCode: String, completion: @escaping (CreatedLocation?, Error?) -> ()) {
+//
+//        reauthenticateUser() { (authCredential, error) in
+//            if let _ = error {
+//                completion(nil, error)
+//        }
+//            guard let authCredential = authCredential else { completion(nil,nil) ; return}
+//        }
+//    }
+//
+//    func deleteLocation() {
+//
+//    }
+//
+//    func fetchLocation() {
+//
+//    }
+//
+//    // MARK: - methods
+//    //with credential: AuthCredential,
+//    private func reauthenticateUser(completion: @escaping (AuthCredential?, Error?) -> ()) {
+//        let user = Auth.auth().currentUser
+//        let credential: AuthCredential
+//        user?.reauthenticate(with: credential, completion: { (credential, error) in
+//            if let error = error {
+//                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+//                completion(nil, error)
+//            } else {
+//                print("User is logged in")
+//                completion(credential?.credential, nil)
+//            }
+//        })
+//    }
 }
 
 
